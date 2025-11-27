1a118515120674f41b30825d36917adf7337786c
madphenixx <loane.gicquel@gmail.com>
2025-11-27 14:34:10 +0100

    fixed animations and projectiles
    the death and hit animation now work and the death screen shows after the death animation has played
    projectiles now self-destruct on cllision with player

---
+++ Assets/Scripts/DestroyBlock.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyBlock : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,19 +2,32 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class PlayerHealth : MonoBehaviour
 {
     public static int playerHealth = 9;
+    public Animator playerAnimator;
+    public float DeathTimer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && playerHealth > 1)
         {
+            
             playerHealth--;
+            playerAnimator.SetTrigger("IsHit");
         }
         else if (playerHealth <= 1)
         {
-            SceneManager.LoadScene("DeathScreen");
+            playerHealth--;
+            playerAnimator.SetTrigger("IsDead");
+
+            
         }
     }
     public void Update()
@@ -22,6 +35,14 @@ public class PlayerHealth : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Return))
         {
             playerHealth--;
+        }
+        if (playerHealth <= 1)
+        {
+            DeathTimer -= Time.deltaTime;
+        }
+        if (DeathTimer <= 0)
+        {
+            SceneManager.LoadScene("DeathScreen");
         }
     }
 }
